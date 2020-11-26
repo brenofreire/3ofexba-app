c35ccb2035296163df987efa78d2df0fb5c012c5
Breno Freire <brenofreire.dev@gmail.com>
2020-11-26 19:57:48 -0300

    chore: adds android platform

--- src/app/services/config/api.config.ts
+++ src/app/services/config/api.config.ts
@@ -1,4 +1,8 @@
-const ENV: 'DEV' | 'PROD' = 'DEV'
-const CURRENT_URL_API = ENV === 'DEV' ? 'http://127.0.0.1:8080/' : ''
+const ENV: 'DEV' | 'PROD' = 'PROD'
+
+const CURRENT_URL_API = {
+    DEV: 'http://127.0.0.1:8080/',
+    PROD: 'https://api-3ofexba.herokuapp.com/'
+}[ENV]
 
 export { CURRENT_URL_API, ENV }
