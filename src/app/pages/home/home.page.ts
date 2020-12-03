f123c66c28027c8442c1e154982db51f01a7f227
Breno Freire <brenofreire.dev@gmail.com>
2020-12-02 21:25:03 -0300

    fix: implements treatment when user has not a org

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -42,10 +42,22 @@ export class HomePage implements OnInit {
   public toMeuCargo() {    
     const meuCargo = this.usuarioCtrl.toMeuCargo({ usuarioLogado: this.usuarioLogado })
 
-    return `
-      E aí meu <b>${meuCargo}</b>, tudo certo?
-      Se liga só no resumo das atividades do seu capítulo!
-    `
+    if(meuCargo) {
+      return `
+        E aí meu <b>${meuCargo || 'brother!'}</b>, tudo certo?
+        Se liga só no resumo das atividades do seu capítulo!
+      `
+    } else {
+      return `
+        E aí meu <b>consagrado</b>, tudo certo?
+        Estamos regularizando sua situação no app. Logo mais você vai ter acesso às informações.<br /><br />
+
+        Lembrando que somente Mestre Conselheiros, Escrivães, Tesoureiros, Hospitaleiros e Lideranças Regionais 
+        terão acesso à este aplicativo por enquanto!<br /><br />
+
+        Valeu? É nois!
+      `
+    }
   }
 
   public async logout() {
