59d8da9e63c4a328594a7f25937502e1eb4ab0a6
Breno Freire <brenofreire.dev@gmail.com>
2021-04-14 21:39:14 -0300

    fix: changes the env because i am a dumbass and forgot to do it

--- src/app/services/config/api.config.ts
+++ src/app/services/config/api.config.ts
@@ -1,8 +1,8 @@
-const ENV: 'DEV' | 'PROD' = 'DEV'
+const ENV: 'DEV' | 'PROD' = 'PROD'
 
 const CURRENT_URL_API = {
-    DEV: 'http://127.0.0.1:3333/',
-    PROD: 'https://api-3ofexba.herokuapp.com/'
+  DEV: 'http://127.0.0.1:3333/',
+  PROD: 'https://api-3ofexba.herokuapp.com/',
 }[ENV]
 
 export { CURRENT_URL_API, ENV }
