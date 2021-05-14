6acb74c6366484053aeb57039b48793c8c5e1ab5
Breno Freire <brenofreire.dev@gmail.com>
2021-05-13 21:16:36 -0300

    fix: infinite scroll on task lists

--- src/app/pages/tarefas/criar-editar-tarefa/criar-editar-tarefa.page.ts
+++ src/app/pages/tarefas/criar-editar-tarefa/criar-editar-tarefa.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { TarefasService } from 'src/app/services/tarefas.service';
-import { cargosEnum } from 'src/app/services/usuario.service';
-import { UtilsService } from 'src/app/services/utils.service';
-import { EditarCriarTarefaPage } from '../../home-admin/editar-criar-tarefa/editar-criar-tarefa.page';
+import { Component, OnInit } from '@angular/core'
+import { ModalController } from '@ionic/angular'
+import { TarefasService } from 'src/app/services/tarefas.service'
+import { cargosEnum } from 'src/app/services/usuario.service'
+import { UtilsService } from 'src/app/services/utils.service'
+import { EditarCriarTarefaPage } from '../../home-admin/editar-criar-tarefa/editar-criar-tarefa.page'
 
 @Component({
   selector: 'app-criar-editar-tarefa',
@@ -15,12 +15,9 @@ export class CriarEditarTarefaPage implements OnInit {
   public termoBusca: string
   public carregando: boolean
   public cargosEnum = cargosEnum
+  public aindaTemResultadoPraCarregar: boolean
 
-  constructor(
-    private tarefasCtrl: TarefasService,
-    private utilsCtrl: UtilsService,
-    private modalCtrl: ModalController,
-  ) { }
+  constructor(private tarefasCtrl: TarefasService, private utilsCtrl: UtilsService, private modalCtrl: ModalController) {}
 
   async ngOnInit() {
     await this.inicializaPagina()
@@ -30,13 +27,13 @@ export class CriarEditarTarefaPage implements OnInit {
     this.carregando = true
     try {
       this.campanhas = await this.tarefasCtrl.getCampanhasAdmin({
-        offset: recarregarDoZero ? 0 : this.campanhas && this.campanhas.length || 0,
-        termoBusca: this.termoBusca || ''
+        offset: recarregarDoZero ? 0 : (this.campanhas && this.campanhas.length) || 0,
+        termoBusca: this.termoBusca || '',
       })
+
+      this.aindaTemResultadoPraCarregar = this.campanhas.length >= 10
     } catch (error) {
-      await this.utilsCtrl.mostrarAlert(
-        'Ops... houve um erro', 'Tente listar campanhas novamente mais tarde'
-      )
+      await this.utilsCtrl.mostrarAlert('Ops... houve um erro', 'Tente listar campanhas novamente mais tarde')
     } finally {
       this.carregando = false
     }
@@ -45,19 +42,40 @@ export class CriarEditarTarefaPage implements OnInit {
   async abrirEditarCriarTarefa(tarefa) {
     const modalEditarCriarTarefa = await this.modalCtrl.create({
       component: EditarCriarTarefaPage,
-      componentProps: { tarefa }
-    })    
-    
+      componentProps: { tarefa },
+    })
+
     await modalEditarCriarTarefa.present()
 
     const { data } = await modalEditarCriarTarefa.onDidDismiss()
 
-    if(data) {
+    if (data) {
       await this.inicializaPagina(true)
     }
   }
 
   async dismiss() {
     await this.modalCtrl.dismiss()
+  }
+
+  async carregarMaisCampanhas(event) {
+    event.target.complete()
+
+    this.carregando = true
+    try {
+      const maisCampanhas: any[] =
+        (await this.tarefasCtrl.getCampanhasAdmin({
+          offset: this.campanhas.length,
+          termoBusca: '',
+        })) || []
+
+      this.aindaTemResultadoPraCarregar = maisCampanhas.length >= 10
+
+      this.campanhas.push(...maisCampanhas)
+    } catch (error) {
+      await this.utilsCtrl.mostrarToast('Ops... houve um erro, Tente listar campanhas novamente mais tarde')
+    } finally {
+      this.carregando = false
+    }
   }
 }
